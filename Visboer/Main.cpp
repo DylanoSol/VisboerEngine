9062f92ce064535b4db06575aef34d8e4818f11d
DylanoSol <dylanosolter@hotmail.com>
2024-04-21 20:26:48 +0200

    [ADDED] OpenGL triangle

--- Visboer/Main.cpp
+++ Visboer/Main.cpp
@@ -6,9 +6,32 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-// settings
+//settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+//Temp hard coded vertex shader
+const char* vertexShaderSource = "#version 460 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//Temp hard coded fragment shader
+const char* fragmentShaderSource = "#version 460 core\n"
+"out vec4 FragColor; \n"
+"void main() \n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);\n"
+"}\0"; 
+
+float vertices[] =
+{
+     -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+}; 
 
 int main()
 {
@@ -41,15 +64,69 @@ int main()
         return -1;
     }
 
+    //OpenGL hello triangle
+
+    //Set up VAO
+    unsigned int VAO; 
+    glGenVertexArrays(1, &VAO); 
+
+    glBindVertexArray(VAO); 
+
+    //Set up VBO
+    unsigned int VBO; 
+    glGenBuffers(1, &VBO); 
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+
+    //Set up vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); 
+    glEnableVertexAttribArray(0); 
+
+    //Set up vertex shader
+    unsigned int vertexShader; 
+    vertexShader = glCreateShader(GL_VERTEX_SHADER); 
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); 
+    glCompileShader(vertexShader); 
+
+    //Set up fragment shader
+    unsigned int fragmentShader; 
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); 
+
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); 
+    glCompileShader(fragmentShader); 
+
+    //Create shader program
+    unsigned int shaderProgram; 
+    shaderProgram = glCreateProgram(); 
+
+    //Set up shader program
+    glAttachShader(shaderProgram, vertexShader); 
+    glAttachShader(shaderProgram, fragmentShader); 
+
+    glLinkProgram(shaderProgram); 
+
+    glUseProgram(shaderProgram); 
+
+    //Delete shaders after linking
+    glDeleteShader(vertexShader); 
+    glDeleteShader(fragmentShader); 
+
     //Main loop
     while (!glfwWindowShouldClose(window))
     {
-      
+    
         processInput(window);
 
         //Clear screen
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        //Draw triangle
+        glUseProgram(shaderProgram); 
+        glBindVertexArray(VAO); 
+        glDrawArrays(GL_TRIANGLES, 0, 3); 
 
         //Swap buffer and poll input output
         glfwSwapBuffers(window);
