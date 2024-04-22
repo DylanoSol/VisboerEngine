e87c5071b6a967fa00e692c2aac1bb2ee2c76199
DylanoSol <dylanosolter@hotmail.com>
2024-04-22 19:34:41 +0200

    [ADDED] Index based drawing

--- Visboer/Main.cpp
+++ Visboer/Main.cpp
@@ -26,12 +26,16 @@ const char* fragmentShaderSource = "#version 460 core\n"
 "   FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);\n"
 "}\0"; 
 
-float vertices[] =
-{
-     -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
-}; 
+float vertices[] = {
+     0.5f,  0.5f, 0.0f,  
+     0.5f, -0.5f, 0.0f,  
+    -0.5f, -0.5f, 0.0f,  
+    -0.5f,  0.5f, 0.0f   
+};
+unsigned int indices[] = {  
+    0, 1, 3,  
+    1, 2, 3   
+};
 
 int main()
 {
@@ -79,9 +83,16 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO); 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
 
+    //Set up indices
+    unsigned int EBO; 
+    glGenBuffers(1, &EBO); 
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); 
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+
     //Set up vertex attributes
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); 
-    glEnableVertexAttribArray(0); 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     //Set up vertex shader
     unsigned int vertexShader; 
@@ -112,6 +123,8 @@ int main()
     //Delete shaders after linking
     glDeleteShader(vertexShader); 
     glDeleteShader(fragmentShader); 
+    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
 
     //Main loop
     while (!glfwWindowShouldClose(window))
@@ -126,12 +139,21 @@ int main()
         //Draw triangle
         glUseProgram(shaderProgram); 
         glBindVertexArray(VAO); 
-        glDrawArrays(GL_TRIANGLES, 0, 3); 
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); 
+
+        //Deselect vertex array
+        glBindVertexArray(0); 
 
         //Swap buffer and poll input output
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    
+    //Deallocate resources
+    glDeleteVertexArrays(1, &VAO); 
+    glDeleteBuffers(1, &VBO); 
+    glDeleteBuffers(1, &EBO); 
+    glDeleteProgram(shaderProgram); 
 
     //Remove all resources
     glfwTerminate();
